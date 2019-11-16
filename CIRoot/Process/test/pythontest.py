736a57574c36d0f672a64f1fb172f99624aab19a
Your Name <you@example.com>
2019-11-16 18:20:39 +0900

    Create pythontest.py

--- CIRoot/Process/test/pythontest.py
+++ CIRoot/Process/test/pythontest.py
@@ -3,5 +3,5 @@ var script="";
 script+=("cd /Library/tomcat/webapps/ROOT/repo;");
 script+=("git init;git remote add origin http://last.dunamisplatform.co.kr:19080/ooranos/cmdb_proto.git;");
 script+=("git pull origin master ;");
-manager.executeShellCommandBg(script,"WW|Username|ooranos,WW|Password|1qazxsw2".aplit(","))
+manager.executeShellCommandBg(script,"WW|Username|ooranos,WW|Password|1qazxsw2".split(","))
 out.println(script)
